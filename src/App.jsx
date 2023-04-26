ab17d4fb6b3072bfa23030539c2da3167641d727
sabrina bendraoua <sabbendra@gmail.com>
2023-04-26 16:17:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  console.log("je suis render");
+
+  const [counter, setCounter] = useState(0);
+
+  const handleCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const updateCounter = () => {
+    setCounter(counter - 1);
+  };
+  const deleteCounter = () => {
+    setCounter(0);
+  };
+  return (
+    <div className="style-app">
+      <section>
+        <button onClick={updateCounter}>-</button>
+        <p
+          className={counter === handleCounter ? handleCounter : updateCounter}
+        >
+          {counter}
+        </p>
+        <button onClick={handleCounter}>+</button>
+      </section>
+      <button onClick={deleteCounter}>Reset</button>
+    </div>
+  );
+};
+
+export default App;
